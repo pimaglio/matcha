9cf2975ee14310f77fed18f814e1b13ffb58c518
Pierre Magliozzi <pimaglio@z1r10p6.le-101.fr>
2019-04-04 15:56:09 +0200

    register

--- view/register.php
+++ view/register.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['loggued_on_user']))
     header("Location: home.php");
 session_start();
 
-include('header.php');
+include('header_alt.php');
 ?>
 
 <body>
@@ -20,7 +20,6 @@ if (isset($_SESSION['error']) && $_SESSION['error'] == 2) {
     echo "<button class='msg-error2'><i class=\"fas fa-exclamation-circle icon\"></i>Ce nom d'utilisateur existe déjà !</button>";
     unset($_SESSION['error']);
 }
-
 
 
 if (isset($_SESSION['error']) && $_SESSION['error'] == 6) {
@@ -54,28 +53,56 @@ if (isset($_SESSION['alert']) && $_SESSION['alert'] == 7) {
 ?>
 <div id="background">
 </div>
-<div class="container-form">
-    <div class="form_pic"></div>
-    <h2 class="title-form">S'inscrire</h2>
-    <form id="register-form" method="POST" action="../controllers/UsersController.php" onsubmit="return isValidForm()">
-        <p class="title-form2">Login</p>
-        <input type="text" name="login" id="login" placeholder="Type your login here" required><br/>
-        <p class="title-form2">Email</p>
-        <input type="email" name="email" id="email" placeholder="Type your E-mail address"
-               required><br/>
-        <p class="title-form2">Password</p>
-        <input type="password" name="password" id="password" class="<?php if ($errmdp == 1) {
-            echo "mdpe";
-        } ?>" placeholder="Type your Password" required><br/>
-        <p class="title-form2">Password (repeat)</p>
-        <input type="password" name="password2" id="repeat_password" class="<?php if ($errmdp == 1) {
-            echo "mdpe";
-        } ?>" placeholder="Repeat your Password"
-               required><br/>
-        <input type="submit" name="submit" value="Register" id="submit" class="btn3"/>
-        <div id="status"></div>
-        <div id="errorContainer"></div>
+
+<div class="form_create_profil">
+    <div class="row">
+        <div class="col s6">
+            <div class="form_pic2"></div>
+        </div>
+        <div class="col s6">
+            <h2 class="title-form">Création <br><span class="title-form-alt">du compte</span></h2>
+        </div>
+    </div>
+    <form id="register-form" method="POST" action="../controllers/ProfilsController.php">
+        <div class="row">
+            <div class="input-field col s12">
+                <i class="material-icons prefix">account_circle</i>
+                <input id="nom" type="text" class="validate" name="nom" required>
+                <label for="nom">Nom et Prénom</label>
+            </div>
+            <div class="input-field col s12">
+                <i class="material-icons prefix">email</i>
+                <input id="email" type="text" class="validate" name="email" required>
+                <label for="email">Adresse email</label>
+            </div>
+            <div class="input-field col s12">
+                <i class="material-icons prefix">vpn_key</i>
+                <input id="password" type="password" class="validate" name="password" required>
+                <label for="password">Mot de passe</label>
+            </div>
+            <div class="input-field col s12">
+                <i class="material-icons prefix">vpn_key</i>
+                <input id="password2" type="password" class="validate" name="password2" required>
+                <label for="password2">Mot de passe (confirmation)</label>
+            </div>
+        </div>
+        <input type="hidden" name="register" value="ok">
+        <div style="text-align: center">
+            <button class="btn-large waves-effect waves-light pink accent-3" type="submit" name="submit"
+                    value="Créer mon profil">S'inscrire
+                <i class="material-icons right">send</i>
+            </button>
+        </div>
     </form>
-    <p class="connect">Tu es déjà inscrit ? <a class="link" href="login.php">Connecte-toi</a></p>
+    <div style="text-align: center; margin-top: 50px">
+        <p class="connect">Tu es déjà inscrit ? <a class="link" href="login.php">Connecte-toi</a></p>
+    </div>
 </div>
+
+<script src="assets/js/materialize.js"></script>
+<script>
+    $(document).ready(function () {
+        $('select').formSelect();
+    });
+</script>
 </body>
